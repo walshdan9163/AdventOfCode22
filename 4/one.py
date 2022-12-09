d47b7e1d46bd1586a5de25e5768ed1479539056b
Daniel Walsh <danielw@ownbackup.com>
2022-12-08 20:06:54 -0500

    :(

--- 4/one.py
+++ 4/one.py
@@ -1,12 +1,26 @@
 import os
 
-from ..helpers import read_input
 
+curr_path = os.path.join(os.getcwd(), '4')
 
 def solve(input_file):
-    input_file_path = os.path.join(os.getcwd(), '4', input_file)
-    for line in read_input(input_file_path):
-        print(line)
+    input_file_path = os.path.join(curr_path, input_file)
+
+    with open(input_file_path) as input:
+        id_pairs = input.read().splitlines()
+
+    overlap_count = 0
+
+    for pair in id_pairs:
+        elf_one_ids, elf_two_ids = pair.split(',')
+        elf_one_ids = elf_one_ids.split('-')
+        elf_two_ids = elf_two_ids.split('-')
+
+        if (elf_one_ids[0] <= elf_two_ids[0] and elf_one_ids[1] >= elf_two_ids[1]) or (elf_two_ids[0] <= elf_one_ids[0] and elf_two_ids[1] >= elf_one_ids[1]):
+            overlap_count += 1
+
+    return overlap_count
+
 
 def main():
     test_result = solve('test_input.txt')
